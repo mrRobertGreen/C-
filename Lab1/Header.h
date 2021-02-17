bd9b3334df1ee420a9cbbbb72b1bca9ba6200435
Robert Green <mrrobertgreen17@gmail.com>
2021-02-17 09:37:14 +0300

    removed unnecessary

--- Lab1/Header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct elem_list {
-	int val;
-	char buf[1024]=""; //это фиктивное поле, используется только для контроля утечки памяти
-	elem_list* next;
-};
-
-typedef elem_list* listptr;
