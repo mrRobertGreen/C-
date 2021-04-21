301f61519bd1a5e39a701e75ee08d3b13f8e854a
Robert Green <mrrobertgreen17@gmail.com>
2021-04-21 13:11:44 +0300

    init

--- indiv2/header.h
+++ indiv2/header.h
@@ -1,4 +1,13 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 
 using namespace std;
+
+class EncryptionKey {
+private:
+	unsigned char* key[256];
+
+	void generate_key();  // TODO: @alex написать генерацию ключа шифрования
+public:
+	EncryptionKey() {};
+};
