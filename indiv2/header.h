8ee5b79526776a0d7ba6a7a8f416d6dc79712cb6
Robert Green <mrrobertgreen17@gmail.com>
2021-04-21 13:30:16 +0300

    Update header.h

--- indiv2/header.h
+++ indiv2/header.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -10,4 +11,25 @@ private:
 	void generate_key();  // TODO: @alex написать генерацию ключа шифрования
 public:
 	EncryptionKey() {};
+
+};
+
+class Encoder : public EncryptionKey {
+public: 
+	string filename;
+private:
+	Encoder(string filename) : filename(filename) {};
+	void encode();
+
+	friend ifstream& operator<<(ifstream& fin, string filename);
+};
+
+class Decoder : public EncryptionKey {
+public:
+	string filename;
+private:
+	Decoder(string filename) : filename(filename) {};
+	void decode();
+
+	friend ostream& operator>>(ofstream& fout, string filename);
 };
