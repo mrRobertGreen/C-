f6e3aa7562235a4288e8f0813427171a58c31f90
Robert Green <mrrobertgreen17@gmail.com>
2021-04-23 16:34:14 +0300

    work

--- indiv2/header.h
+++ indiv2/header.h
@@ -7,19 +7,17 @@ using namespace std;
 class EncryptionKey {
 private:
 	unsigned char* key[256];
-
-	void generate_key();  // TODO: @alex написать генерацию ключа шифрования
 public:
 	EncryptionKey() {};
-
 };
 
 class Encoder : public EncryptionKey {
 public: 
 	string filename;
 private:
-	Encoder(string filename) : filename(filename) {};
+	Encoder(string filename) : filename(filename) { EncryptionKey(); };
 	void encode();
+	Decoder& create_decoder();
 
 	friend ifstream& operator<<(ifstream& fin, string filename);
 };
--- indiv2/source.cpp
+++ indiv2/source.cpp
@@ -1,4 +1,22 @@
-#include <iostream>
+﻿#include <iostream>
 #include "header.h"
 
 using namespace std;
+
+EncryptionKey::EncryptionKey() {
+	// TODO: @alex написать генерацию ключа шифрования
+}
+
+void Encoder::encode() {
+	fstream file(filename, fstream::in, fstream::out);
+	if (!file.is_open()) {
+		cerr << "Can't open " << filename << "\n";
+		return;
+	}
+	char symb;
+	string data;
+	while (file.get(symb)) {
+		data.push_back(symb);
+		
+	}
+}
