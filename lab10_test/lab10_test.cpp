3f698d24aa2114045622b8d42c116748e17b0eb5
Robert Green <mrrobertgreen17@gmail.com>
2021-04-29 16:33:05 +0300

    finish lab10

--- lab10_test/lab10_test.cpp
+++ lab10_test/lab10_test.cpp
@@ -9,11 +9,11 @@ namespace lab10test
 	TEST_CLASS(lab10test)
 	{
 	public:
-		
+
 		TEST_METHOD(delFirstTest)
 		{
 			List list;
-			for (int i = 0; i < 5; ++i)	
+			for (int i = 0; i < 5; ++i)
 				list.addFirst(i);
 			list.delFirst();
 			Assert::AreEqual(*list.begin(), 3);
@@ -21,7 +21,7 @@ namespace lab10test
 		TEST_METHOD(delLastTest)
 		{
 			List list;
-			for (int i = 0; i < 5; ++i)	
+			for (int i = 0; i < 5; ++i)
 				list.addLast(i);
 			list.delLast();
 			ListIterator iter = list.begin();
@@ -32,28 +32,40 @@ namespace lab10test
 		TEST_METHOD(for_each_test)
 		{
 			List list;
-			for (int i = 0; i < 5; ++i)	
+			for (int i = 0; i < 5; ++i)
 				list.addLast(i);
 			list.for_each(mult2);
 			ListIterator iter = list.begin();
 			for (int i = 0; i < 5; ++i)
 			{
-				Assert::AreEqual(*iter, i*2);
+				Assert::AreEqual(*iter, i * 2);
 				++iter;
-			}	
+			}
 		}
 		TEST_METHOD(countTest)
 		{
 			List list;
-			for (int i = 0; i < 5; ++i)	
+			for (int i = 0; i < 5; ++i)
 				list.addLast(i);
 			list.for_each(mult2);
 			ListIterator iter = list.begin();
 			for (int i = 0; i < 5; ++i)
 			{
-				Assert::AreEqual(*iter, i*2);
+				Assert::AreEqual(*iter, i * 2);
 				++iter;
-			}	
+			}
+		}
+		TEST_METHOD(iteratorOperatorPlusTest)
+		{
+			List list;
+			for (int i = 0; i < 5; ++i)
+				list.addLast(i);
+			ListIterator iter = list.begin();
+			Assert::AreEqual(*iter, 0);
+			Assert::AreEqual(*(iter + 1), 1);
+			Assert::AreEqual(*(iter + 2), 2);
+			Assert::AreEqual(*(iter + 3), 3);
+			Assert::AreEqual(*(iter + 4), 4);
 		}
 	};
 }
--- lab_10/main.cpp
+++ lab_10/main.cpp
@@ -4,70 +4,60 @@
 using namespace std;
 
 int main() {
-
-	//List list;
-	//for (int i = 4; i > -1; i--)
-	//	list.addFirst(i);
-
-	//for (int i = 5; i < 10; i++)
-	//	list.addLast(i);
-	//cout << list;
-
-	//cout << "Count of odd: " << list.count(odd) << "\n";
-
-	//list.for_each(mult2);
-	//cout << "After x2 operation:\n";
-	//cout << list;
-
-	//cout << "Count of odd: " << list.count(odd) << "\n";
-
-	//ListIterator iter = list.begin();
-
-	//cout << "Print with using iterator:\n";
-	//while (iter != list.end())
-	//{
-	//	cout << *iter << " ";
-	//	++iter;
-	//}
-
-	//for (int i = 0; i < 5; ++i)
-	//	list.delLast();
-	//cout << "\nAfter removing 5 last elements:\n";
-	//cout << list << "\n";
-
-	//iter = list.begin() + 2;
-	//try
-	//{
-	//	list.insert(999, iter);
-	//}
-	//catch (const std::exception& err)
-	//{
-	//	cerr << err.what() << "\n";
-	//}
-	//cout << "After inserting 999 before 3rd position:\n";
-	//cout << list << "\n";
-
-	//iter = list.begin() + 2;
-	//try
-	//{
-	//	list.remove(iter);
-	//}
-	//catch (const std::exception& err)
-	//{
-	//	cerr << err.what() << "\n";
-	//}
-
-	//cout << "After removing item from 3rd position:\n";
-	//cout << list << "\n";
-
 	List list;
-	for (int i = 0; i < 5; ++i)
+	for (int i = 4; i > -1; i--)
+		list.addFirst(i);
+
+	for (int i = 5; i < 10; i++)
 		list.addLast(i);
-	list.delLast();
-	cout << list << "\n";
+	cout << list;
+
+	cout << "Count of odd: " << list.count(odd) << "\n";
+
+	list.for_each(mult2);
+	cout << "After x2 operation:\n";
+	cout << list;
+
+	cout << "Count of odd: " << list.count(odd) << "\n";
+
 	ListIterator iter = list.begin();
-	while (iter + 1 != list.end())
+
+	cout << "Print with using iterator:\n";
+	while (iter != list.end())
+	{
+		cout << *iter << " ";
 		++iter;
+	}
+
+	for (int i = 0; i < 5; ++i)
+		list.delLast();
+	cout << "\nAfter removing 5 last elements:\n";
+	cout << list << "\n";
+
+	iter = list.begin() + 2;
+	try
+	{
+		list.insert(999, iter);
+	}
+	catch (const std::exception& err)
+	{
+		cerr << err.what() << "\n";
+	}
+	cout << "After inserting 999 before 3rd position:\n";
+	cout << list << "\n";
+
+	iter = list.begin() + 2;
+	try
+	{
+		list.remove(iter);
+	}
+	catch (const std::exception& err)
+	{
+		cerr << err.what() << "\n";
+	}
+
+	cout << "After removing item from 3rd position:\n";
+	cout << list;
 
 	return EXIT_SUCCESS;
 }
