ed6152bbd72e1726d8ead0ce853b927a4c3edfd3
Robert Green <mrrobertgreen17@gmail.com>
2021-05-13 12:55:08 +0300

    test equal template function from lectures

--- for_tests/header.h
+++ for_tests/header.h
@@ -2,3 +2,14 @@
 #include <iostream>
 
 using namespace std;
+
+template<typename InIt1, typename InIt2>
+bool my_equal(InIt1 first, InIt1 last, InIt2 first2) {
+	while (first != last) {
+		if (*first != *first2)
+			return false;
+		++first;
+		++first2;
+	}
+	return true;
+}
--- for_tests/main.cpp
+++ for_tests/main.cpp
@@ -1,10 +1,27 @@
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 #include "header.h"
 
 using namespace std;
 
 int main() {
+	// для векторов алгоритм equal работает некорректно в двух случаях
+	// 1) когда у векторов разная длина
+	// 2) когда у векторов одинаковая начальная длина, но потом она меняется
+	/*vector<int> a(3);
+	for (int i = 0; i < 3; i++)
+		a.push_back(i);
 
+	vector<int> b(3);
+	for (int i = 0; i < 4; i++)
+		b.push_back(i);*/
+
+	// для обычных статических массивов алгоритм equal работает некорректно, если у массивов разная длина
+	int a[3] = { 1, 2, 3 }; 
+	int b[4] = { 1, 2, 3, 4 };
+	
+
+	cout << my_equal(begin(a), end(a), begin(b));
 
 	return EXIT_SUCCESS;
 }
